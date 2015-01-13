0d691d8e105013a25ac80643f32c1457c72015c7
qiaoxin <qiaoxin265@126.com>
2015-01-13 11:35:36 +0800

    添加环信SDK---iOS版

--- proj.ios/AppController.mm
+++ proj.ios/AppController.mm
@@ -1,6 +1,7 @@
 #import "AppController.h"
 #import "AppDelegate.h"
 #import "RootViewController.h"
+#import "EaseMob.h"
 
 @implementation AppController
 
@@ -19,8 +20,17 @@ static AppDelegate s_sharedApplication;
     window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
     
     // Init the EAGLView
+    
 
-    // Use RootViewController manage EAGLView 
+    /*环信注册*/
+    //注册 APNS文件的名字, 需要与后台上传证书时的名字一一对应
+    NSString *apnsCertName = @"chatdemo";
+    [[EaseMob sharedInstance] registerSDKWithAppKey:@"easemob-demo#chatdemo" apnsCertName:apnsCertName];
+    // 需要在注册sdk后写上该方法
+    [[EaseMob sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+
+     
+    // Use RootViewController manage EAGLView
     viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
     viewController.wantsFullScreenLayout = NO;
 
@@ -45,12 +55,68 @@ static AppDelegate s_sharedApplication;
     return YES;
 }
 
+//自定义方法
+- (void)registerRemoteNotification
+{
+#if !TARGET_IPHONE_SIMULATOR
+    UIApplication *application = [UIApplication sharedApplication];
+    
+    //iOS8 注册APNS
+    if ([application respondsToSelector:@selector(registerForRemoteNotifications)]) {
+        [application registerForRemoteNotifications];
+        UIUserNotificationType notificationTypes = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:notificationTypes categories:nil];
+        [application registerUserNotificationSettings:settings];
+    }else{
+        UIRemoteNotificationType notificationTypes = UIRemoteNotificationTypeBadge |
+        UIRemoteNotificationTypeSound |
+        UIRemoteNotificationTypeAlert;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notificationTypes];
+    }
+    
+#endif
+}
+
+//系统方法
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    //SDK方法调用
+    [[EaseMob sharedInstance] application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+//系统方法
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    //SDK方法调用
+    [[EaseMob sharedInstance] application:application didFailToRegisterForRemoteNotificationsWithError:error];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"注册推送失败"
+                                                    message:error.description
+                                                   delegate:nil
+                                          cancelButtonTitle:@"确定"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+//系统方法
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //SDK方法调用
+    [[EaseMob sharedInstance] application:application didReceiveRemoteNotification:userInfo];
+}
+
+//系统方法
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    //SDK方法调用
+    [[EaseMob sharedInstance] application:application didReceiveLocalNotification:notification];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
+    [[EaseMob sharedInstance] applicationWillResignActive:application];
     CrossApp::CAApplication::getApplication()->pause();
 }
 
@@ -58,6 +124,7 @@ static AppDelegate s_sharedApplication;
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+     [[EaseMob sharedInstance] applicationDidBecomeActive:application];
     CrossApp::CAApplication::getApplication()->resume();
 }
 
@@ -66,6 +133,7 @@ static AppDelegate s_sharedApplication;
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
      */
+    [[EaseMob sharedInstance] applicationDidEnterBackground:application];
     CrossApp::CCApplication::sharedApplication()->applicationDidEnterBackground();
 }
 
@@ -73,6 +141,7 @@ static AppDelegate s_sharedApplication;
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
+     [[EaseMob sharedInstance] applicationWillEnterForeground:application];
     CrossApp::CCApplication::sharedApplication()->applicationWillEnterForeground();
 }
 
@@ -81,6 +150,7 @@ static AppDelegate s_sharedApplication;
      Called when the application is about to terminate.
      See also applicationDidEnterBackground:.
      */
+    [[EaseMob sharedInstance] applicationWillTerminate:application];
 }
 
 
