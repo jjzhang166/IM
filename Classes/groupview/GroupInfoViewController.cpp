748ea28a04102098a1af756e936a8af4fff70d96
chenq <chenq@lfwu.local>
2015-01-19 09:31:21 +0800

    群信息界面

---
+++ Classes/groupview/GroupInfoViewController.cpp
@@ -0,0 +1,9 @@
+//
+//  GroupInfoViewController.cpp
+//  IM
+//
+//  Created by chenq on 15-1-16.
+//
+//
+
+#include "GroupInfoViewController.h"
---
+++ Classes/groupview/GroupInfoViewController.h
@@ -0,0 +1,14 @@
+//
+//  GroupInfoViewController.h
+//  IM
+//
+//  Created by chenq on 15-1-16.
+//
+//
+
+#ifndef __IM__GroupInfoViewController__
+#define __IM__GroupInfoViewController__
+
+#include <iostream>
+
+#endif /* defined(__IM__GroupInfoViewController__) */
