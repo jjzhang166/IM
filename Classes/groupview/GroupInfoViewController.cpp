572e9b41fb2ddbc24cbcbab55b4657f93fe7f320
qiaoxin <qiaoxin265@126.com>
2015-01-23 11:18:35 +0800

    no message

--- Classes/groupview/GroupInfoViewController.cpp
+++ Classes/groupview/GroupInfoViewController.cpp
@@ -9,7 +9,6 @@
 #include "GroupInfoViewController.h"
 #include "CrossApp.h"
 #include "RootWindow.h"
-#include "IMDATA.h"
 
 GroupInfoViewController::GroupInfoViewController()
 {};
@@ -18,21 +17,18 @@ GroupInfoViewController::~GroupInfoViewController()
 {};
 
 
-GroupInfoViewController* GroupInfoViewController::create(GroupInfo info,bool joined)
+GroupInfoViewController* GroupInfoViewController::create()
 {
     GroupInfoViewController* controller = new GroupInfoViewController();
-    controller->init(info,joined);
+    controller->init();
     
     controller->autorelease();
     return controller;
 };
 
-bool GroupInfoViewController::init(GroupInfo info,bool joined)
+bool GroupInfoViewController::init()
 {
     if (CAViewController::init()) {
-        m_info = info ;
-        m_joined = joined;
-        
         CABarButtonItem* backItem = CABarButtonItem::create("back", NULL, NULL);
         backItem->setTarget(this, CAControl_selector(GroupInfoViewController::onButtonBack));
         CANavigationBarItem* navigation = CANavigationBarItem::create(UTF8("群信息"));
@@ -61,7 +57,7 @@ void GroupInfoViewController::viewDidLoad()
     CAView* _headView = CAView::createWithFrame(CCRect(0, 0, winRect.size.width, 562), CAColor_white);
     CCRect _headCC = _headView->getBounds() ;
     m_imgViewHead = CAImageView::createWithCenter(CCRect(_headCC.size.width/2, 166, 150, 150));
-    m_imgViewHead->setImage(m_info.faceImg);
+    m_imgViewHead->setImage(CAImage::create("HelloWorld.png"));
     _headView->addSubview(m_imgViewHead);
     
     CAView* _lineView = CAView::createWithFrame(CCRect(_px(40), 336, winRect.size.width-_px(80), _px(1)), ccc4(200, 200, 200, 255));
@@ -74,10 +70,11 @@ void GroupInfoViewController::viewDidLoad()
     _headView->addSubview(topicLabel);
     
     m_LabelTopic = CALabel::createWithFrame(CCRect(240, 372, 300, 39));
-    m_LabelTopic->setText(m_info.topic->getCString());
+    m_LabelTopic->setText(UTF8("主题??"));
     m_LabelTopic->setFontSize(_px(38));
     m_LabelTopic->setColor(ccc4(51, 51, 51, 255));
     _headView->addSubview(m_LabelTopic);
+    
     
     CAView* _lineView2 = CAView::createWithFrame(CCRect(_px(40), 450, winRect.size.width-_px(80), _px(1)), ccc4(200, 200, 200, 255));
     _headView->addSubview(_lineView2);
@@ -89,8 +86,7 @@ void GroupInfoViewController::viewDidLoad()
     _headView->addSubview(introduceLabel);
     
     m_LabelIntroduce = CALabel::createWithFrame(CCRect(240, 490, 300, 39));
-//    m_LabelIntroduce->setText(UTF8("介绍是什么"));
-    m_LabelIntroduce->setText(m_info.introduce->getCString());
+    m_LabelIntroduce->setText(UTF8("介绍是什么"));
     m_LabelIntroduce->setFontSize(_px(38));
     m_LabelIntroduce->setColor(ccc4(51, 51, 51, 255));
     _headView->addSubview(m_LabelIntroduce);
@@ -109,8 +105,7 @@ void GroupInfoViewController::viewDidLoad()
     _memberView->addSubview(ownerLabel);
     
     m_LabelOwner = CALabel::createWithFrame(CCRect(240, 40, 200, 39));
-//    m_LabelOwner->setText(UTF8("群主姓名"));
-    m_LabelOwner->setText(m_info.owner->getCString());
+    m_LabelOwner->setText(UTF8("群主姓名"));
     m_LabelOwner->setColor(ccc4(51, 51, 51, 255));
     m_LabelOwner->setFontSize(_px(38));
     _memberView->addSubview(m_LabelOwner);
@@ -126,8 +121,7 @@ void GroupInfoViewController::viewDidLoad()
     _memberView->addSubview(memberLabel);
     
     m_LabelMember = CALabel::createWithFrame(CCRect(240, 152, 200, 39));
-    m_LabelMember->setText(UTF8("成员人数"));
-//    m_LabelMember->setText(*m_info.);
+    m_LabelMember->setText(UTF8("成员姓名"));
     m_LabelMember->setColor(ccc4(51, 51, 51, 255));
     m_LabelMember->setFontSize(_px(38));
     _memberView->addSubview(m_LabelMember);
@@ -146,8 +140,7 @@ void GroupInfoViewController::viewDidLoad()
     _limitView->addSubview(limitLabel);
     
     m_LabelLimit = CALabel::createWithFrame(CCRect(240, 40, 400, 39));
-//    m_LabelLimit->setText(UTF8("无需验证即可加入"));
-    m_LabelLimit->setText(m_info.limit->getCString());
+    m_LabelLimit->setText(UTF8("无需验证即可加入"));
     m_LabelLimit->setColor(ccc4(51, 51, 51, 255));
     m_LabelLimit->setFontSize(_px(38));
     _limitView->addSubview(m_LabelLimit);
@@ -163,11 +156,12 @@ void GroupInfoViewController::viewDidLoad()
     noticeLabel->setFontSize(_px(38));
     _noticeView->addSubview(noticeLabel);
 
-    m_SwitchNotice = CASwitch::createWithFrame(CCRect(winRect.size.width - 171, 23, 130, 80));
-//    m_SwitchNotice->setOnImage(CAImage::create(SWITCH_ON));
-//    m_SwitchNotice->setOffImage(CAImage::create(SWITCH_OFF));
-    m_SwitchNotice->setIsOn(m_info.noticeOn, true);
-    _noticeView->addSubview(m_SwitchNotice);
+//    m_LabelOwner = CALabel::createWithFrame(CCRect(240, 40, 200, 39));
+//    m_LabelOwner->setText(UTF8("群主姓名"));
+//    m_LabelOwner->setColor(ccc4(51, 51, 51, 255));
+//    m_LabelOwner->setFontSize(_px(38));
+//    _memberView->addSubview(m_LabelOwner);
+    
     _noticeView->addSubview(_lineView3);
     
     CALabel* gmemberLabel = CALabel::createWithFrame(CCRect(_px(40), 152, 150, 39));
@@ -178,24 +172,16 @@ void GroupInfoViewController::viewDidLoad()
 
     scrollView->addSubview(_noticeView);
 
-    
     m_ExitButton = CAButton::createWithFrame(CCRect(_px(40), 1572, winRect.size.width-_px(80), 110), CAButtonTypeRoundedRect);
-       if (m_joined) {
-        m_ExitButton->setTitleForState(CAControlStateAll, UTF8("退出群聊"));
-        CAImage * btnBG = CAImage::create(BUTTON_EXIT_GROUP );
-        CAImageView * imgView = CAImageView::create();
-        imgView->setImage(btnBG);
-        m_ExitButton->setBackGroundViewForState(CAControlStateAll,imgView);
-    } else{
-        m_ExitButton->setTitleForState(CAControlStateAll, UTF8("加入群聊"));
-        CAImage * btnBG = CAImage::create(BUTTON_JOIN_GROUP );
-        CAImageView * imgView = CAImageView::create();
-        imgView->setImage(btnBG);
-        m_ExitButton->setBackGroundViewForState(CAControlStateAll,imgView);
-    }
+    m_ExitButton->setTitleForState(CAControlStateAll, UTF8("退出群聊"));
     m_ExitButton->setTitleColorForState(CAControlStateAll, CAColor_white);
-
+    CAImage * btnBG = CAImage::create("btn_exit.png");
+    CAImageView * imgView = CAImageView::create();
+    imgView->setImage(btnBG);
+    m_ExitButton->setBackGroundViewForState(CAControlStateAll,imgView);
     scrollView->addSubview(m_ExitButton);
+    
+    
 
     scrollView->setViewSize(CCSize(winRect.size.width,1737));
     this->getView()->addSubview(scrollView);
@@ -204,15 +190,9 @@ void GroupInfoViewController::viewDidLoad()
 void GroupInfoViewController::viewDidUnLoad()
 {};
 
-// 返回按钮
-void GroupInfoViewController:: onButtonBack(CAControl* target, CCPoint point)
+
+void GroupInfoViewController:: onButtonBack(CAControl* target, CCPoint)
 {
     RootWindow::getInstance()->getNavigationController()->popViewControllerAnimated(this);
     
 };
-// 消息提醒
-void GroupInfoViewController:: onSwitchChanged(CAControl* target, CCPoint point)
-{};
-// 退群按钮
-void GroupInfoViewController:: onButtonExit(CAControl* target, CCPoint point)
-{};
