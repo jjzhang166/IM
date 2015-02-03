089ac74a84143c0b01235f7ff1a0074c002e89a6
qiaoxin <qiaoxin265@126.com>
2015-02-03 12:06:56 +0800

    远程忽略文件

--- proj.android/obj/local/armeabi/objs/CrossApp_static/basics/CAFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/CrossApp_static/basics/CAFPSImages.o: \
- D:/CrossApp/CrossApp/basics/CAFPSImages.c \
- D:/CrossApp/CrossApp/basics/CAFPSImages.h
-
-D:/CrossApp/CrossApp/basics/CAFPSImages.h:
--- proj.android/obj/local/armeabi/objs/CrossApp_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/CrossApp_static/kazmath/src/neon_matrix_impl.o: \
- D:/CrossApp/CrossApp/kazmath/src/neon_matrix_impl.c \
- D:/CrossApp/CrossApp/kazmath/include/kazmath/neon_matrix_impl.h
-
-D:/CrossApp/CrossApp/kazmath/include/kazmath/neon_matrix_impl.h:
