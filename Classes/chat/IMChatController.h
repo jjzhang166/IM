e2e2822278c9947472a7137328b419243d74369b
chinahypo <weihaibo_yjy@163.com>
2015-03-19 10:46:39 +0800

    添加聊天界面，数据未初始化

---
+++ Classes/chat/IMChatController.h
@@ -0,0 +1,51 @@
+﻿
+#ifndef __IMChat__Controller__
+#define __IMChat__Controller__
+
+#include <iostream>
+#include "CrossApp.h"
+#include "CrossAppExt.h"
+
+USING_NS_CC;
+USING_NS_CC_EXT;
+
+
+class IMChatController : public CAViewController, public CATableViewDelegate, public CATableViewDataSource
+{
+
+public:
+
+	IMChatController(std::string ID);
+
+	virtual ~IMChatController();
+
+	static IMChatController* create(std::string _id);
+
+protected:
+
+	void viewDidLoad();
+
+	void viewDidUnload();
+
+public:
+	virtual void tableViewDidSelectRowAtIndexPath(CATableView* table, unsigned int section, unsigned int row);
+	virtual void tableViewDidDeselectRowAtIndexPath(CATableView* table, unsigned int section, unsigned int row);
+
+	virtual CATableViewCell* tableCellAtIndex(CATableView* table, const CCSize& cellSize, unsigned int section, unsigned int row);
+	// 	virtual CAView* tableViewSectionViewForHeaderInSection(CATableView* table, const CCSize& viewSize, unsigned int section);
+	// 	virtual CAView* tableViewSectionViewForFooterInSection(CATableView* table, const CCSize& viewSize, unsigned int section);
+	virtual unsigned int numberOfRowsInSection(CATableView *table, unsigned int section);
+	virtual unsigned int numberOfSections(CATableView *table);
+	virtual unsigned int tableViewHeightForRowAtIndexPath(CATableView* table, unsigned int section, unsigned int row);
+	virtual unsigned int tableViewHeightForHeaderInSection(CATableView* table, unsigned int section);
+	virtual unsigned int tableViewHeightForFooterInSection(CATableView* table, unsigned int section);
+
+
+private:
+	CADipSize size;
+	CATableView* p_TableView;
+	std::string id;
+};
+
+
+#endif /* defined(__HelloCpp__ViewController__) */
